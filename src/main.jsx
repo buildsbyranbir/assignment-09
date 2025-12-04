675928ace867fbb7a8a8c4c616b7583c03a3dc76
Ranbir <ranbir.webdev@gmail.com>
2025-12-04 23:13:22 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import AuthProvider from './providers/AuthProvider.jsx'
 // Import global styles if any, mostly handled by CDN tailwind but this ensures React builds correctly
 // import './index.css' 
